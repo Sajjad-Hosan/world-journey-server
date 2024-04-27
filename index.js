1b81b1840fda28cd2e72f053aa9b914e23799d17
Sajjad-Hosan <s4jj4dH034n@gmail.com>
2024-04-27 07:49:54 +0600

    tour post,get and user post,get programe code

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =`mongodb+srv://${process.env.MONO_NAME}:${process.env.MONO_KEY}@cluster0.wrggdnr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONO_NAME}:${process.env.MONO_KEY}@cluster0.wrggdnr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,14 +21,37 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // await client.connect();
+    const touristsCollection = client
+      .db("World_Journeys")
+      .collection("tourists");
+    const usersCollection = client.db("World_Journeys").collection("users");
+    // show all tourists spots
     app.get("/tourists", async (req, res) => {
-      const touristsCollection = client
-        .db("World_Journeys")
-        .collection("tourists");
       const result = await touristsCollection.find().toArray();
       res.send(result);
     });
+    // get tourist details card datas
+    app.post("/tourist", async (req, res) => {
+      const details = req.body;
+      const result = await touristsCollection.insertOne(details);
+      res.send(result);
+    });
+    // get one tourist details form database
+    app.get("/tourist/:id", async (req, res) => {
+      const id = req.params.id;
+      const tourist = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const result = await touristsCollection.findOne(filter);
+      res.send(result);
+    });
+    // get user details who is entering
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    //
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
